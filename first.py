763b57139788153ba2bc089f3a43284b449f72a1
abdul wassy <mabdulwassaykhan@gmail.com>
2025-10-29 22:32:48 +0500

    Python Learning

---
+++ first.py
@@ -0,0 +1,42 @@
+print("Hello World")
+print(3,2,1)
+print(3+2+1)
+#  Printing number and string
+'''
+print("name ",3+3)
+'''
+
+
+A = 22
+B = 33
+C = A + B
+print(C)
+
+
+age = 23
+name = "Khan"
+
+print(name,":",age)
+print(age)
+
+A,B,C = "Orange","Banana","Apple"
+
+print(A,B,C)
+
+Fruit = ["Orange","Banana","Apple"]
+print(Fruit)
+
+X=range(10)
+print(X)
+print(type(Fruit))
+
+Student={
+    "Name":"M.Abdul Wassay Khan",
+    "Reg#":"MC-BSCS-243-007",
+    "":"",
+    "":"",
+    "":"",
+    "":"",
+}
+
+
