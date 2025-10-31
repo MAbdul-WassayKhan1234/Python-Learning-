27ba1dfc33605800c1fbb06e6853e89708a9d2f0
abdul wassy <mabdulwassaykhan@gmail.com>
2025-10-31 10:35:31 +0500

    Py Basics continue

--- first.py
+++ first.py
@@ -1,35 +1,59 @@
+# ##################################### Getting Started with Python ############################################
+
+# Install Python from https://www.python.org/downloads/
+# Install IDE (Integrated Development Environment) like VSCode
+# Create a new Python file with .py extension (e.g., first.py)
+# open terminal or command prompt
+# Navigate to the directory where your Python file is located using 'cd' and press tab key for auto-completion
+# PS C:\Users\HC\Desktop\.py>  python first.py
+# PS C:\Users\HC\Desktop\.py> python --version
+
+##################################### Getting Started with Python ############################################
+
+
+# ### Python Basics                   
+
+
+# Printing Hello World
 print("Hello World")
+
+# Printing multiple values
 print(3,2,1)
+
+# Printing the sum of values
 print(3+2+1)
+
 #  Printing number and string
-'''
 print("name ",3+3)
-'''
 
 
+# Variables
 A = 22
 B = 33
 C = A + B
 print(C)
 
-
+# Printing name and age
 age = 23
 name = "Khan"
-
 print(name,":",age)
 print(age)
 
+# Multiple variable assignment
 A,B,C = "Orange","Banana","Apple"
-
 print(A,B,C)
 
+# List of fruits
 Fruit = ["Orange","Banana","Apple"]
-print(Fruit) 
-
-X=range(10)
-print(X)
+print(Fruit)
 print(type(Fruit))
 
+# Range of numbers
+X=range(10)
+print(X)
+print(type(X))
+
+# Dictionary to store student information
 Student={
     "Name":"M.Abdul Wassay Khan",
     "Reg#":"MC-BSCS-243-007",
@@ -42,6 +66,61 @@ print(Student)
 print(type(Student))
 print(Student["Name"])
 print(len(Student))
+
+
+
+
+
+
+
+
+
+
+
+# print("Hello World")
+# print(3,2,1)
+# print(3+2+1)
+# #  Printing number and string
+# '''
+# print("name ",3+3)
+# '''
+
+
+# A = 22
+# B = 33
+# C = A + B
+# print(C)
+
+
+# age = 23
+# name = "Khan"
+
+# print(name,":",age)
+# print(age)
+
+# A,B,C = "Orange","Banana","Apple"
+
+# print(A,B,C)
+
+# Fruit = ["Orange","Banana","Apple"]
+# print(Fruit) 
+
+# X=range(10)
+# print(X)
+# print(type(Fruit))
+
+# Student={
+#     "Name":"M.Abdul Wassay Khan",
+#     "Reg#":"MC-BSCS-243-007",
+#     "CGPA":"3.75",
+#     "Email":"mawk@example.com",
+#     "Phone":"123-456-7890",
+#     "Address":"123 Main St, Multan, Pakistan",
+# }
+# print(Student)
+# print(type(Student))
+# print(Student["Name"])
+# print(len(Student))
 
 
 
