4a07177c3f8ed0e7a81bf8ab9d32cf1290aa2f73
abdul wassy <mabdulwassaykhan@gmail.com>
2025-11-04 16:34:01 +0500

    Python Basics Continue

--- MC-BSCS-243-007.py
+++ MC-BSCS-243-007.py
@@ -95,5 +95,17 @@ age = input("Enter your age: ")
 print("Name:",name)
 print("Age:",age)
 
+# Python Operators
+x = 10
+y = 3
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Floor Division:", x // y)
+print("Modulus:", x % y)
+print("Exponentiation:", x ** y)
+
+
 
 # #<!-------------------------------------- End of Python Basics ------------------------------------------!># #
